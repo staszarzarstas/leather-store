5ce7d871e0294a84df4909b898812c7de05c219d
Alina <AliKochneva@yandex.ru>
2023-05-16 11:46:24 +0700

    кнопка возврата наверх страницы при скроле страницы вниз

---
+++ js/backToTopButton.js
@@ -0,0 +1,23 @@
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+    if (scrollContainer().scrollTop > showOnPx) {
+        backToTopButton.classList.remove("hidden")
+    } else {
+        backToTopButton.classList.add("hidden")
+    }
+})
+
+//возвращение наверх
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: "smooth",
+    });
+};
+
+backToTopButton.addEventListener("click", goToTop)
