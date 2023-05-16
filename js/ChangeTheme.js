eebe88c0cb4d30ad575c121e29bb6c35dfca601f
Alina <AliKochneva@yandex.ru>
2023-05-16 21:26:00 +0700

    вывод текста о смене темы на экран из js

--- js/ChangeTheme.js
+++ js/ChangeTheme.js
@@ -1,5 +1,7 @@
 //смена темы на светлую / тёмную
 
+const text = document.querySelector('.ThemeInfo')
+
 document.querySelector('.themetoggle').addEventListener('click', (event) => {
     event.preventDefault();
     if (localStorage.getItem('theme') === 'light') {
@@ -15,9 +17,12 @@ function addlightClassToHTML() {
     try {
     if (localStorage.getItem('theme') === 'light') {
         document.querySelector('html').classList.add('light');
+        text.innerText = 'тёмная тема'
     }
     else {
         document.querySelector('html').classList.remove('light');
+        text.innerText = 'светлая тема'
+
     }
     } catch (err) { }
 }
