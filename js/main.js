8554bfa71052cf86a663fa6ee1f42434416ca1e3
Alina <AliKochneva@yandex.ru>
2023-05-17 16:12:57 +0700

    проверка валидности формы

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-//скрол ссылок
 
-//посмотреть новое меню
+
+//скрол для меню
 document.getElementById('main-info').onclick = function () {
-    document.getElementById('sofas').scrollIntoView({behavior: 'smooth'})
+    document.getElementById('products').scrollIntoView({behavior: 'smooth'})
 }
 
 //ссылки из header
+
 let links = document.querySelectorAll('.menu-item > a')
 for (let i = 0; i < links.length; i++){
     links[i].onclick = function () {
@@ -13,52 +14,36 @@ for (let i = 0; i < links.length; i++){
     }
 }
 
-//ссылки из footer
 
-let linksfooter = document.querySelectorAll('.footer-item > a')
-for (let i = 0; i < linksfooter.length; i++){
-    linksfooter[i].onclick = function () {
-        document.getElementById(linksfooter[i].getAttribute("data-link")).scrollIntoView({behavior: 'smooth'})
-    }
-}
+//валидация инпутов для отправки данных менеджеру
 
+const helpInfo = document.querySelector('.help-form-inputs')
+const helpText = document.querySelector('.help-form-text')
+const helpButton = document.querySelector('#help-action')
+const helpContent = document.querySelector('#helpText')
+const person = document.querySelector('#name')
+const phone = document.querySelector('#phone')
 
+helpButton.addEventListener('click', function () {
 
+    let hasError = false
+    const info = [helpContent, person, phone]
 
-
-//кнопки "заказать"
-// let buttons = document.getElementsByClassName('product-button')
-// for (let i = 0; i < buttons.length; i++){
-//     buttons[i].onclick = function () {
-//         document.getElementById("order").scrollIntoView({behavior: 'smooth'})
-//     }
-// }
-
-// //валидация инпутов
-// const buttonOrder = document.querySelector('#order-action')
-// const burger = document.querySelector('#burger')
-// const person = document.querySelector('#name')
-// const phone = document.querySelector('#phone')
-
-// buttonOrder.addEventListener('click', function () {
-
-//     let hasError = false
-//     const info = [burger, person, phone]
-
-//     info.forEach(item => {
-//         if (!item.value){
-//             item.parentElement.style.background = 'red'
-//             hasError = true
+    info.forEach(item => {
+        if (!item.value){
+            item.parentElement.style.background = 'red'
+            hasError = true
             
-//         } else {
-//             item.parentElement.style.background = ''
-//         }
-//     })
+        } else {
+            item.parentElement.style.background = ''
+        }
+    })
 
-//     if (!hasError){
-//         info.forEach(item => {
-//             item.value = ''
-//         })
-//         alert('Спасибо за заказ!')
-//     }
-// })
+    if (!hasError){
+        info.forEach(item => {
+            item.value = ''
+        })
+        helpInfo.classList.add('none')
+        helpText.innerText = 'Мы скоро свяжемся с вами!'
+    }
+})
