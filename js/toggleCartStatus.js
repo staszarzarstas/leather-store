083625cc5d15ec82e7194070aa6da882e4ccfd99
Alina <AliKochneva@yandex.ru>
2023-05-18 08:41:35 +0700

    обноавление статуса корзины

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -20,34 +20,30 @@ function calcCartPriceAndDelivery() {
 	const totalPriceEl = document.querySelector('.total-price');
 	const deliveryCost = document.querySelector('.delivery-cost');
 	const cartDelivery = document.querySelector('[data-cart-delivery]');
+    const deliveryText = document.querySelector('.deliveryText')
 
-	// Общая стоимость товаров
 	let priceTotal = 0;
 
-	// Обходим все блоки с ценами в корзине
 	priceElements.forEach(function (item) {
-		// Находим количество товара
 		const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
-		// Добавляем стоимость товара в общую стоимость (кол-во * цену)
 		priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
 	});
 
-	// Отображаем цену на странице
 	totalPriceEl.innerText = priceTotal;
 
-	// Скрываем / Показываем блок со стоимостью доставки
 	if (priceTotal > 0) {
 		cartDelivery.classList.remove('none');
 	} else {
 		cartDelivery.classList.add('none');
 	}
 
-	// Указываем стоимость доставки
-	if (priceTotal >= 600) {
+	if (priceTotal >= 50000) {
 		deliveryCost.classList.add('free');
 		deliveryCost.innerText = 'бесплатно';
+        deliveryText.innerText = ''
 	} else {
 		deliveryCost.classList.remove('free');
-		deliveryCost.innerText = '250 ₽';
+		deliveryCost.innerText = '2500 ₽';
+        deliveryText.innerText = 'Бесплатно при заказе от 50000 ₽'
 	}
 }
