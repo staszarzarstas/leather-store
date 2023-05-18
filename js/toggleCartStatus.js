abf45a9ba4cd85140cfc2854e59489323f03f788
Alina <AliKochneva@yandex.ru>
2023-05-18 11:10:27 +0700

    стилизация формы

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -3,13 +3,16 @@ function toggleCartStatus() {
     const cartWrapper = document.querySelector('.cart-wrapper');
     const cartEmptyBadge = document.querySelector('[data-cart-empty]');
     const orderForm = document.querySelector('#order-form');
+	const total = document.querySelector('.emptyBucket')
 
     if (cartWrapper.children.length > 0) {
         cartEmptyBadge.classList.add('none');
         orderForm.classList.remove('none');
+		total.classList.remove('none')
     } else {
         cartEmptyBadge.classList.remove('none');
         orderForm.classList.add('none');
+		total.classList.add('none')
     }
 
 }
